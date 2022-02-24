7f4553bb10c5c8748b8262cc4690008d70c50c79
luniont <1511491804@qq.com>
2022-02-24 20:18:52 +0800

    feat(hot-recommend): 完成热门推荐的样式和数据渲染

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.jsx
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.jsx
@@ -1,16 +1,34 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 
-import LYFThemeHeaderRCM from 'components/theme-header-rcm'
+import { HOT_RECOMMEND_LIMIT } from '@/common/constants'
+import LYFThemeHeaderRCM from '@/components/theme-header-rcm'
+import LYFSongsCover from '@/components/songs-cover'
 import { HotRecommendWrapper } from './style'
-
+import { getHotRecommendsAction } from '../../store/actionCreators'
 export default memo(function LYFHotRecommend() {
+  const { hotRecommends } = useSelector(
+    (state) => ({
+      hotRecommends: state.getIn(['recommend', 'hotRecommends'])
+    }),
+    shallowEqual
+  )
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getHotRecommendsAction(HOT_RECOMMEND_LIMIT))
+  }, [dispatch])
   return (
     <HotRecommendWrapper>
       <LYFThemeHeaderRCM
         title="热门推荐"
         keywords={['华语', '流行', '民谣', '摇滚', '电子']}
       />
-      <div className="recommend-list"></div>
+      <div className="recommend-list">
+        {hotRecommends.map((item, index) => {
+          return <LYFSongsCover key={item.id} info={item}></LYFSongsCover>
+        })}
+      </div>
     </HotRecommendWrapper>
   )
 })
--- src/pages/discover/c-pages/recommend/store/actionCreators.js
+++ src/pages/discover/c-pages/recommend/store/actionCreators.js
@@ -1,17 +1,29 @@
-import * as actionTypes from './constants'
+import * as actionTypes from './constants';
 
-import { getTopBanners } from '@/service/recommend'
+import { getTopBanners, getHotRecommends } from '@/service/recommend'
 
 const changeTopBannerAction = (res) => ({
   type: actionTypes.CHANGE_TOP_BANNERS,
   topBanners: res.banners
-})
+});
 
+const changeHotRecommendAction = (res) => ({
+  type: actionTypes.CHANGE_HOT_RECOMMEND,
+  hotRecommends: res.result
+})
 
 export const getTopBannerAction = (res) => {
   return dispatch => {
     getTopBanners().then(res => {
       dispatch(changeTopBannerAction(res))
+    })
+  }
+}
+
+export const getHotRecommendsAction = (limit) => {
+  return dispatch => {
+    getHotRecommends(limit).then(res => {
+      dispatch(changeHotRecommendAction(res))
     })
   }
 }
